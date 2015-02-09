f5d19430d40161dc4b08a86e4b80f2ae1b987835
fuglsang <volmer@gmail.com>
2015-02-09 23:16:48 +0100

    added win32 binaries and icon

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,19 @@
 #include "ofApp.h"
 
 //========================================================================
-int main( ){
-
-	//ofSetupOpenGL(ofApp::viewportWidth,ofApp::viewportHeight,OF_WINDOW); // <-------- setup the GL context
-	ofSetupOpenGL(ofApp::viewportWidth,ofApp::viewportHeight,OF_FULLSCREEN); // <-------- setup the GL context
+int main(int argc, char ** argv)
+{
+	if (argc > 1 && strcmp(argv[1], "-f") == 0)
+	{
+		ofSetupOpenGL(ofApp::viewportWidth, ofApp::viewportHeight, OF_FULLSCREEN); // <-------- setup the GL context
+	}
+	else
+	{
+		ofSetupOpenGL(ofApp::viewportWidth, ofApp::viewportHeight, OF_WINDOW); // <-------- setup the GL context
+	}
 	
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofRunApp( new ofApp());
-
+	ofRunApp(new ofApp());
 }
